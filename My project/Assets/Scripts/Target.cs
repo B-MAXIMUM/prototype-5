0a748b559e5d8cc6fd09ff00d399956634004102
B-MAXIMUM <bgmundy5223@gmail.com>
2022-11-09 11:02:22 -0800

    gay

--- My project/Assets/Scripts/Target.cs
+++ My project/Assets/Scripts/Target.cs
@@ -6,20 +6,29 @@ public class Target : MonoBehaviour
 {
     public int PointValue = 1;
     public float MinSpeed = 10;
-    public float MaxSpeed = 20;
-    public float MaxTorque= 10;
+    public float MaxSpeed = 15;
+    public float MaxTorque= 100000000000000;
     private Rigidbody2D _targetRb;
 
     // Start is called before the first frame update
     void Start()
     {
         _targetRb = GetComponent<Rigidbody2D>();
-        _targetRb.AddForce(Vector2.up * MinSpeed, ForceMode2D.Impulse);
+        _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
+        _targetRb.AddTorque(RandomizeTorque());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private float RandomizeForce()
+    {
+        return Random.Range(MinSpeed, MaxSpeed);
+    }
+    private float RandomizeTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
     }
 }
