c3691a3be9824227dceb743ea7c7d16e59613a98
B-MAXIMUM <bgmundy5223@gmail.com>
2022-11-10 11:03:51 -0800

    added thibdfdg

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool SuperSexTime = true;
     public List<GameObject> Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    IEnumerator SpawnTarget()
+    {
+        while(SuperSexTime)
+        {
+            yield return new WaitForSeconds(1);
+            int index = Random.Range(0, Target.Count);
+            Instantiate(Target[index]);
+        }
         
     }
 }
